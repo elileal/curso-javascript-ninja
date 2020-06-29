baac80e1c9334cb6dbd00e936fc9e35d66ca2577
Eliabe Leal <elyabeleal@yahoo.com.br>
2020-06-29 18:39:11 -0300

    Challenge 07

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -8,15 +8,15 @@ Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
 function addItem(item) {
-  return arr.push(item);
+  arr.push(item);
+  return arr;
 }
 
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-addItem([true, [false, true], 'Eu']);
-console.log(arr);
+console.log(addItem([true, [false, true], 'Eu']));
 
 
 /*
@@ -57,8 +57,8 @@ Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
 console.log('Números ímpares entre 10 e 20:');
 var init = 10;
-while (init <= 20) {
-  (init % 2 === 1) ? console.log(init) : '';
+while (init < 20) {
+  (init % 2 !== 0) ? console.log(init) : '';
   init++;
 }
 
